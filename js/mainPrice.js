ec891f378867a463449458fa4fbf764278675f80
sana6622 <90204420+sana6622@users.noreply.github.com>
2022-06-12 21:23:04 +0800

    修改背景路徑

--- js/mainPrice.js
+++ js/mainPrice.js
@@ -27,11 +27,11 @@ $(document).ready(function () {
     $(this).parents().siblings('.areaAns').toggle();
     let a = $(this).attr('src')
     console.log(a)
-    if (a === '/img/icon_plus.png') {
-      $(this).attr('src', 'img/icon_minus.png')
+    if (a === 'img/icon_plus.png') {
+      $(this).attr('src', '../img/icon_minus.png');
       $(this).siblings().addClass('changeColor')
     } else {
-      $(this).attr('src', 'img/icon_plus.png')
+      $(this).attr('src', '../img/icon_plus.png');
       $('.areaQus p').removeClass('changeColor')
     }
   })
